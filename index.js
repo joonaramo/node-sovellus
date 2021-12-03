fd0995239542e9d76b49e47b8ce23911959ccfe8
joonaramo <joona00ramo@gmail.com>
2021-12-03 13:55:45 +0200

    db connection

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const mysql = require('mysql');
 const port = process.env.PORT || 8080;
 
+const config = {
+  host: 'mydb.tamk.fi',
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
+  connectionLimit: 10,
+};
+
 app.use(cors());
+
+const pool = mysql.createPool(config);
+
+app.get('/', (req, res) => {
+  pool.query('SELECT * from locations', (error, results) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.send(results);
+    }
+  });
+});
 
 const db = [{ name: 'tiina' }, { name: 'jack' }];
 
